36c4ded032f5f49448aabde45a18f40b047633e5
PierreGent <pierregentile42@gmail.com>
2018-03-25 15:47:13 +0200

    Ajout de la prmière version de l'app (la plupart des menus de l'application)

---
+++ app/src/main/java/com/example/gentile/lexical/MainProf.java
@@ -0,0 +1,13 @@
+package com.example.gentile.lexical;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainProf extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_prof);
+    }
+}
